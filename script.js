d3f9e0975b1ebd5ddcc41c63adb49f94648aec4c
maomira <141223548+maomira@users.noreply.github.com>
2023-08-02 18:35:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-window.addEventListener("scroll", function () {
-    const parallax = document.querySelector(".parallax .background-image");
-    let scrollPosition = window.pageYOffset;
-    parallax.style.transform = `translateY(${scrollPosition * 0.5}px)`;
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector(".button");
+    const secondBackground = document.querySelector(".black-background");
+
+    button.addEventListener("click", function () {
+        // Calculate the distance to scroll based on the second background position
+        const scrollDistance = secondBackground.getBoundingClientRect().top;
+
+        // Smoothly scroll to the second background using the ScrollToOptions behavior
+        window.scrollTo({
+            top: scrollDistance,
+            behavior: "smooth",
+        });
+    });
 });
